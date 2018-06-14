9c6b1f834a40959e4f7b4cf162c55e7e001be677
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-14 17:57:20 +0100

    Create shift.js

---
+++ commands/shift.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message, args) => {
+    message.delete();
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry pal, you can't do that.");
+
+  
+    //>ogłoszenie [message here]
+        let announceembed = new Discord.RichEmbed()
+        .setTitle(`AN INTERVIEW HAS STARTED!`)
+        .setThumbnail("https://t4.rbxcdn.com/7f8703c61b1e1d59b840f8c81ec54707")
+        .setDescription(`**Hosted by:**${message.author}\n\nThere’s currently a **Shift Session** starting! Come and have a nice refreshing drink and a luxurious room.\n\n:link: JOIN HERE:\nhttps://web.roblox.com/games/1914949051/SUMMER-HOTEL-Snowflakes-Hotel `)
+        .setColor("#ff6a00")
+        .setTimestamp()
+
+        let announceChannel = message.guild.channels.find(`name`, `sessions`);
+        if(!announceChannel) return message.channel.send("Couldn't find sessions channel.");
+        
+        announceChannel.send(`${everyone}`,announceembed);
+}
