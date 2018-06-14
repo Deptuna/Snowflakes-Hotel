9410492a8a8504f0a6610b12118a235c6dda6f87
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-14 16:18:40 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,10 @@
-
     const Discord = require('discord.js')
-exports.run = async (client, message, args, color) => {
-
-        let API = (client.ping).toFixed(2)
-        let embed = new Discord.RichEmbed()
-        .setTitle(`:ping_pong: ${API}ms!`)
-        .setColor(0xff2f2f)
-        message.channel.send(embed);
-
-}
+    exports.run = async (client, message, args, color) => {
+    
+            let embed = new Discord.RichEmbed()
+            .setTitle(`:ping_pong:`+Math.round(client.ping)+`ms!`)
+            .setColor(0xff2f2f)
+            message.channel.send(embed);
+    
+    }
+    
