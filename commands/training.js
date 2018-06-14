ca457396ca32814884d886b77384fed8e129e663
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-14 16:55:57 +0100

    Update and rename trainings.js to training.js

--- commands/training.js
+++ commands/training.js
@@ -9,11 +9,11 @@ module.exports.run = async (bot,message, args) => {
         let announceembed = new Discord.RichEmbed()
         .setTitle(`A TRAINING HAS STARTED!`)
         .setThumbnail("https://t2.rbxcdn.com/7ee1a4609d47281e36c92c140df9df82")
-        .setDescription(`**Hosted by:**${message.author}\n\nThere’s currently a **Training Session** starting! If you’re looking to rank up in Snowflakes Hotel, then why not give it a shot at an Trainings?\n\nWe will be starting in **15 minutes.**\n\n:link: JOIN HERE:\nhttps://t2.rbxcdn.com/7ee1a4609d47281e36c92c140df9df82`)
+        .setDescription(`**Hosted by:**${message.author}\n\nThere’s currently a **Training Session** starting! If you’re looking to rank up in Snowflakes Hotel, then why not give it a shot at an Trainings?\n\nWe will be starting in **15 minutes.**\n\n:link: JOIN HERE:\nhttps://web.roblox.com/games/1901320327/Snowflakes-Hotel-Training-Centre`)
         .setColor("#ff6a00")
         .setTimestamp()
 
-        let everyone = message.guild.roles.find('name', "@everyone");
+        let everyone = message.guild.roles.find('name', "@{- Public Announcements -}");
         let announceChannel = message.guild.channels.find(`name`, `sessions`);
         if(!announceChannel) return message.channel.send("Couldn't find sessions channel.");
         
