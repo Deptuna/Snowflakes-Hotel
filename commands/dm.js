36d5e0b6e166166cef9b6db7fe1b4963320ca616
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 22:15:53 +0100

    Delete dm.js

--- commands/dm.js
+++
@@ -1,7 +0,0 @@
-   module.exports.run = async (bot,message, args) => {
-   
-   setInterval(function() {
-        let user = message.mentions.users.first()
-            user.send('LMAO LOL')
-    }, 1)
-}
