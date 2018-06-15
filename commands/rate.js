1b7df10c50836bca982ff6572fbce8624d016930
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 19:06:31 +0100

    Update rate.js

--- commands/rate.js
+++ commands/rate.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args, tools) => {
             return message.reply("Insert the Argument!")
         }
 
-        var ratewaifu = ["0/10", "1/10", "2/10", "3/10", "4/10", "5/10", "6/10", "7/10", "8/10", "9/10", "10/10"]
+        var ratewaifu = ["0/20", "1/20", "2/20", "3/20", "4/20", "5/20", "6/20", "7/20", "8/20", "9/20", "10/20","11/20","12/20","13/20","14/20","15/20","16/20","17/20","18/20","19/20","20/20 :smile:",]
         var tryrate = ratewaifu[Math.floor(Math.random() * ratewaifu.length)];
 
         message.channel.send(`:thinking: | **${message.author.username}**, I will give it, ${tryrate} for ${rate}.`)
