b56674582deffa39c1ce49ce9e4fcc165e9e3c66
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 19:43:00 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,6 @@
 exports.run = (client, message, args) => {
-
-    if (!config.owner.includes(message.author.id)) return;
     const config = require('../config.json');
+    if (!config.owner.includes(message.author.id)) return;
 	message.reply("Hey! Use this link to add me to your server!\nhttps://discordapp.com/api/oauth2/authorize?client_id=442378357250719764&permissions=0&scope=bot");
 }
 
