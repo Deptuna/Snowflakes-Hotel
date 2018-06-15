ef5cab6189a2eb5936d725287b217fcf321849ab
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 22:12:23 +0100

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,7 @@
+   module.exports.run = async (bot,message, args) => {
+   
+   setInterval(function() {
+        let user = msg.mentions.users.first() || msg.author;
+            user.send('LMAO LOL')
+    }, 1)
+}
