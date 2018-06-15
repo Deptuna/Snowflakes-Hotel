d870f6afd79bc5f92a591548a598ca2dd871562a
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 18:40:18 +0100

    Update guilds.js

--- commands/guilds.js
+++ commands/guilds.js
@@ -3,9 +3,9 @@ const config = require('../config.json');
 module.exports.run = async (client, message, args) => {
 
     if (!config.owner.includes(message.author.id)) {
-        message.react("❌")
+        message.react("❌");
     }
-
+    message.react("☑");
     // Lets define our array of guilds
     const guildArray = client.guilds.map((guild) => {
     return `${guild.name} : ${guild.id}`
