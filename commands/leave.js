95e9ac19494213c77746892a7d1a447dba90bd95
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 19:18:10 +0100

    Create leave.js

---
+++ commands/leave.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const cfg = require('../config.js');
+
+exports.execute = (bot, message, args) => {
+  if(message.author.id != bot.config.OWNER_ID){
+      const embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setDescription(`Only bot owner can use this command`)
+      message.channel.send({embed});
+      return;
+  }
+  if(args.length < 1){
+    const embed = new Discord.RichEmbed()
+    .setColor(cfg.config.wb)
+    .setDescription(`No value specified.\n\nUsage: ${cfg.config.PREFIX}leaveguild <guild.id>`)
+    message.channel.send({embed});
+    return;
+  }
+  if(args.length > 1){
+    return;
+  }
+  var id = args[0]
+  var guildsmap = bot.guilds.map(g => g.id)
+    if(!guildsmap.includes(args[0])){
+      const embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setDescription(`I'm not a member of this guild.`)
+      message.channel.send({embed});
+      return;
+    }
+  bot.guilds.get(id).leave()
+  const embed = new Discord.RichEmbed()
+  .setColor(`RANDOM`)
+  .setDescription(`Successfully left from guild.`)
+  message.channel.send({embed});
+}
