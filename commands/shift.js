c61dc7421f7163a6cfc5fa9b99db56b251ca2383
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 21:24:21 +0100

    Update shift.js

--- commands/shift.js
+++ commands/shift.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot,message, args) => {
     //>ogłoszenie [message here]
         let announceembed = new Discord.RichEmbed()
         .setTitle(`AN INTERVIEW HAS STARTED!`)
-        .setThumbnail("https://t4.rbxcdn.com/7f8703c61b1e1d59b840f8c81ec54707")
-        .setDescription(`**Hosted by:**${message.author}\n\nThere’s currently a **Shift Session** starting! Come and have a nice refreshing drink and a luxurious room.\n\n:link: JOIN HERE:\nhttps://web.roblox.com/games/1914949051/SUMMER-HOTEL-Snowflakes-Hotel `)
+        .setThumbnail("https://t7.rbxcdn.com/f7eb97097909ef22a8b39ce44e9435ae")
+        .setDescription(`**Hosted by:**${message.author}\n\nThere’s currently a **Shift Session** starting! Come and have a nice refreshing drink and a luxurious room.\n\n:link: JOIN HERE:\nhttps://web.roblox.com/games/1901006193/Snowflakes-Hotel-V1#`)
         .setColor("#ff6a00")
         .setTimestamp()
 
