079276637d66058aa9ed20917fbfac96f53a32f1
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-15 18:42:51 +0100

    Update guilds.js

--- commands/guilds.js
+++ commands/guilds.js
@@ -1,16 +1,21 @@
-const config = require('../config.json');
 
-module.exports.run = async (client, message, args) => {
+exports.run = (client, message, args, tools) => {
 
-    if (!config.owner.includes(message.author.id)) {
-        message.react("❌");
-    }
-    message.react("☑");
-    // Lets define our array of guilds
-    const guildArray = client.guilds.map((guild) => {
-    return `${guild.name} : ${guild.id}`
+    if(message.author.id !== "335430609860296705")message.react("❌");
+        message.react("☑");
+    const os = require('os');
+    const arch = os.arch()
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+
+    let totalSeconds = process.uptime();
+    let realTotalSecs = Math.floor(totalSeconds % 60);
+    let days = Math.floor((totalSeconds % 31536000) / 86400);
+    let hours = Math.floor((totalSeconds / 3600) % 24);
+    let mins = Math.floor((totalSeconds / 60) % 60);
+var ping = client.ping
+
+    message.author.send(`\n= Memory usage: ${Math.round(used * 100) / 100}MB\n= Ping: ${ping}\n= Uptime: Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${realTotalSecs}\n= Node: ${process.version}\n= Library: discord.js\n= ARCH: ${arch}\n= Plataform: ${os.platform}\n= Servers: ${client.guilds.size}\n= Users: ${client.users.size}`, {
+        code: 'AsciiDoc'
     })
-  
-    // And send it
-    message.author.send(`= Guild List =\n\`\`\`${guildArray.join("\n")}\`\`\``, {code:'asciidoc'})
-  }
+
+}
