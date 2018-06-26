e347ce18c6a8b6cf157680963afa710766ff141c
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-26 15:07:23 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,6 @@ module.exports.run = async (bot, message, args) => {
       color: 0x1D82B6,
       description: `**Check your DMs ${message.author}!**`
   }})
-    message.author.send("Here Is my command list!")
     let modembed = new Discord.RichEmbed()
     .setColor("#15f153")
     .addField("Moderation commands:", "``addrole``: Adds the user the desired role.\n``purge``: Deletes the number of messages you choose.\n``removerole``: Removes a user the desired role.\n``mute``: Mutes a user with a time which you choose.\n``announce``: Announces your desired choice.\n``training``: Announces that you are hosting a training session.\n``interview``: Announces that you are hosting an interview session.\n``shift``: Announces that you are hosting a shift session.")
