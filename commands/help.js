07432cdff2ca52a58de7ba08d2d2f37ac4f093cb
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-06-26 15:13:34 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,16 +9,13 @@ module.exports.run = async (bot, message, args) => {
   }})
     let modembed = new Discord.RichEmbed()
     .setColor("#15f153")
-    .addField("Moderation commands:", "``addrole``: Adds the user the desired role.\n``purge``: Deletes the number of messages you choose.\n``removerole``: Removes a user the desired role.\n``mute``: Mutes a user with a time which you choose.\n``announce``: Announces your desired choice.\n``training``: Announces that you are hosting a training session.\n``interview``: Announces that you are hosting an interview session.\n``shift``: Announces that you are hosting a shift session.")
+    .addField("Moderation commands:", "``addrole``: Adds the user the desired role.\n``purge``: Deletes the number of messages you choose.\n``removerole``: Removes a user the desired role.\n``mute``: Mutes a user with a time which you choose.\n``announce``: Announces your desired choice.\n``training``: Announces that you are hosting a training session.\n``interview``: Announces that you are hosting an interview session.\n``shift``: Announces that you are hosting a shift session.");
     let userembed = new Discord.RichEmbed()
     .setColor("#15f153")
-     .addField("User commands","``8ball``\n``avtar``\n``botinfo``\n``dog``\n``say``\n``flip``\n``createinvite``\n``membercount``\n``serverinfo``\n``ping``\n``rate``\n``ship``\n``roll``\n``dogfact``\n``catfact``\n``should``\n``weather``\n``randomcolor``")
-    
-    return message.author.send(userembed);
+     .addField("User commands","``8ball``\n``avtar``\n``botinfo``\n``dog``\n``say``\n``flip``\n``createinvite``\n``membercount``\n``serverinfo``\n``ping``\n``rate``\n``ship``\n``roll``\n``dogfact``\n``catfact``\n``should``\n``weather``\n``randomcolor``");
+        
     message.author.send(modembed);
-    
-  
-    
+    return message.author.send(userembed);
   }
  
  
