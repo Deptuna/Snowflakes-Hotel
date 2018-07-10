dbfd45d2e9a29bd13bf41a60b52a6c76758dc8ae
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-07-10 17:08:41 +0100

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,17 +1,15 @@
  const Discord = require("discord.js");
  const config = require('../config.json');
- const index = require('../index.js');
 
 module.exports.run = async (client,message,args) => {
 
- const token = index.token;
 
  if (!config.owner.includes(message.author.id)) return;
 
 
  message.channel.send('Restarting...')
      client.destroy()
-    .then(() => client.login(token))
+    .then(() => client.login(process.env.token);
     .then(() => message.channel.send("Restarted!"))
     .then(() => console.log("Restarted"));
   }
