938fec0d5d609052ce6ad7fbe7503b4d532c4990
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-26 18:12:25 +0300

    Delete add.js

--- commands/add.js
+++
@@ -1,9 +0,0 @@
-const Discord = module.require("discord.js");
-
-exports.run = async (bot, message, args) => {
-        let numArray = args.map(n=> parseInt(n));
-        let total = numArray.reduce( (p, c) => p+c);
-
-        message.channel.sendMessage(`**Add : ${numArray}**, ` + `Total = ${total}`);
-
-}
