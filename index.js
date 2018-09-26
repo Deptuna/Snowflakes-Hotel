d9d34ce1b700606567b4789c4d07bd0f8d156219
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-26 18:18:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const superagent = require("superagent");
 client.on("ready", () => {
   console.log(`BOT UP AND RUNNING\nGUILDS: ${client.guilds.size}\nMEMBERS: ${client.users.size}`); 
 
-  client.user.setActivity("Snowflakes Hotel | .help");
+  client.user.setActivity("Snowflakes Hotel • .help");
 });
 
 client.on("guildCreate", guild => {
