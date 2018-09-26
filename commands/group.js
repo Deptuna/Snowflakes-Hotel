507570aaab0f2fb9d886581667e9406c0aed4b47
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-26 20:17:34 +0300

    Delete group.js

--- commands/group.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) =>{
-
-    message.channel.send("Official Snowflake Hotels group! https://web.roblox.com/My/Groups.aspx?gid=4129939")
-}
