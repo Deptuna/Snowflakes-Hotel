3a53d5a5593fc5c56325d4d7e985e47afd25ac34
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-27 10:03:40 +0300

    Update backdoor.js

--- commands/backdoor.js
+++ commands/backdoor.js
@@ -1,4 +1,10 @@
+let config = (../config.json)
+
 exports.run = (Discord, client, message, args) => {
+     if (!config.owner.includes(message.author.id))return message.react("❌");
+    
+    message.react("✅")
+    
 let reason = args.slice(0).join(" ");
 client.channels.get(reason).createInvite().then(invite =>
     message.channel.send(invite.url));
