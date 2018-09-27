0cf31a3e18d81c77980a1f111e8fbf5f2b6afd91
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-27 09:27:11 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,20 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
-module.exports.run = async (bot, message, args) => {
-  if (!args[0]) return message.channel.send(`Usage: .say [anything]`);
+module.exports.run = async (client, message, args) => {
 
-  message.delete();
-  let embedarg = args.slice(0).join(' ')
-message.channel.send(embedarg);
+ 
+   
+    const sayMessage = args.join(' ');
+    if(!args[0]) return;
+    message.delete()
+   
+    const embed = new Discord.RichEmbed()
+   
+    .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL)
+    .setDescription(sayMessage)
+    .setTimestamp()
+    .setColor(0x36393F)
+   
+    message.channel.send(embed)
 }
+
