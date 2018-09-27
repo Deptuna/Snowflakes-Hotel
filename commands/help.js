edb0dc9b82990da773ee4fc9c215985af89b7ffd
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-27 08:42:11 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   }})
     let Discord = new Discord.RichEmbed()
     .setColor("#15f153") 
-     .addField("Commands","``interview``\n``training``\n``shift``\n``avatar``\n``botinfo``\n``dog``\n``say``\n``flip``\n``membercount``\n``serverinfo``\n``ping``\n``rate``\n``ship``\n``dogfact``\n``catfact``\n``should``\n``weather``\n``randomcolor``/n``rps``/n``timer``/n``report``");
+     .addField("Commands","``interview``\n``training``\n``shift``\n``avatar``\n``botinfo``\n``dog``\n``say``\n``flip``\n``membercount``\n``serverinfo``\n``ping``\n``rate``\n``ship``\n``dogfact``\n``catfact``\n``should``\n``weather``\n``randomcolor``/n``rps``/n``timer``/n``report``")
         
     
     message.author.send(Discord);
