ac918b6ae6f4817363b47ab9e98a4c54e764ab17
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-27 09:04:57 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
     message.react("☑");
     message.channel.send({embed: {
       color: 0x1D82B6,
-      description: `**There is currently a problem with the command. ${message.author}!**`
+      description: `**Check your Dms ${message.author}!**`
   }})
     let embed = new Discord.RichEmbed()
     .setColor("#15f153") 
-    .addField("Commands","``interview``\n``training``\n``shift``\n``avatar``\n``botinfo``\n``dog``\n``say``\n``flip``\n``membercount``\n``serverinfo``\n``ping``\n``rate``\n``ship``\n``dogfact``\n``catfact``\n``should``\n``weather``\n``randomcolor``/n``rps``/n``timer``/n``report``")
+    .addField("Commands","``interview``\n``training``\n``shift``\n``8ball``\n``avatar``\n``botinfo``\n``dog``\n``say``\n``flip``\n``membercount``\n``serverinfo``\n``ping``\n``rate``\n``ship``\n``dogfact``\n``catfact``\n``should``\n``weather``\n``randomcolor``\n``rps``\n``timer``\n``report``")
         
     
    message.author.send(embed);
