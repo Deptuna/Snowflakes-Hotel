44820ca4a45ead14760a99be942df4b170302155
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-30 19:38:29 +0300

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Owner", "￵￶￵ ￶£ndo ¶erl#9595 (335430609860296705)")
+    .addField("Co-Owner", " ig.DrFatCafe#0001 (453227176280457267)")
     .addField("Bot Name", bot.user.username)
     .addField("Created On", `Sat May 05 2018 `)
     .setFooter("Requested by: "+message.author.tag, message.author.avatarURL)
