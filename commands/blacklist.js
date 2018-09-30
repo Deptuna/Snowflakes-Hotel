a09f252659fb07545b8fc91f21643ebb75aa97d4
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-09-30 12:46:20 +0300

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,1 +1,14 @@
 
+let Blacklisted = new Discord.RichEmbed()
+        .setTitle(":no_entry_sign: Blacklisted")
+        .setDescription(`You've been blacklisted from using Zeta, for at least one of the following reasons:\n 1) Misue Of Zeta.\n 2) Stolen Zeta Assets.\n 3) Or any other non-stated reason.`)
+        .setFooter("If you'd like to appeal to be whitelisted please contact,£ndo ¶erl#9595")
+        .setColor("RED")
+        .setThumbnail(message.author.avatarURL);
+      
+      const blacklisted1 = ["495889956523212802"]
+      const blacklisted2 = [""]
+      
+     if(message.content.startsWith(prefix)) {
+         if(message.author.id == blacklisted1, blacklisted2) return message.channel.send(Blacklisted); 
+      }
