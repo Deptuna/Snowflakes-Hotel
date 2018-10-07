7059d8184c326ab7fd9990d6d3c5514864c9a66c
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-10-07 15:34:05 +0300

    Update setavatar.js

--- commands/setavatar.js
+++ commands/setavatar.js
@@ -1,6 +1,6 @@
 const config = require('../config.json');
 exports.run = (client, message, args) => {
-    if (!config.owner.includes(message.author.id)) return;
+    if (!config.prime.includes(message.author.id)) return;
     let newavatartime = message.content.split(" ").slice(1);
     let blasty = newavatartime.join(" ");
     if(!blasty.startsWith('http')){
