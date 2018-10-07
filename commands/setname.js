5ce757ef348692b14e1c3e7fe18c1f21963c1c8e
Not Minecraft daa <39019879+somerandomdood@users.noreply.github.com>
2018-10-07 15:32:35 +0300

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 
 exports.run = (client, message, args) => {
-    if (!config.owner.includes(message.author.id)) return;
+    if (!config.prime.includes(message.author.id)) return;
 
     let doit = message.content.split(" ").slice(1);
     let pikapi = doit.join(" ");
